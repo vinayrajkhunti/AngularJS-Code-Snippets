e8bae11650e42aebcbc0616aca4bfe59607b108e
vinayraj khunti <vinayrajkhunti@users.noreply.github.com>
2016-04-18 11:21:40 -0700

    Update direxample.js

--- Custom-Directive/direxample.js
+++ Custom-Directive/direxample.js
@@ -1,1 +1,14 @@
+var app = angular.module('myApp',[' ']);
+
+
+app.directive('helloFriend', function () {
+        return {
+            restrict: 'E',
+            scope:{
+                name:'bind'
+            },
+            template: '<h1> Hi {{name}} </h1>'
+
+        }
+    })
 
