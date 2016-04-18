2128f12326d6f53a0950b60867cfe01ca5831741
vinayraj khunti <vinayrajkhunti@users.noreply.github.com>
2016-04-18 11:34:08 -0700

    Update filterex.js

--- Custom-Filters/filterex.js
+++ Custom-Filters/filterex.js
@@ -1,1 +1,26 @@
+var app = angular.module('myApp',[]);
 
+app.controller('myCtrl',function($scope,$http)
+  {
+			$scope.products = [];
+			$http.get('filter.json')
+			.success(function(resp){
+				$scope.products = resp;
+			})
+		});
+		
+		app.filter('capitalize',function(){
+			//THIS CUSTOM FILTER CAPITALIZE FIRST LETTER OF STRING
+				var result = input;
+				return result.charAt(0).toUpperCase()+result.substring(1);
+			};
+		});
+		// TO REVERSE THE STRING, WE CAN USE 'reverse' CUSTOM FILTER, DEMONSTRATED AS BELOW.
+		/*app.filter('reverse',function()
+		{
+  				return function(result){
+  				  var result = $scope.employees;
+						return result.name.split('').reverse().join('')
+  		};
+
+  	});*/
