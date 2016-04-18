dbed20f7fd9b7db6de458d6e9add4571be5dad89
vinayraj khunti <vinayrajkhunti@users.noreply.github.com>
2016-04-18 12:04:13 -0700

    Update exfactory.js

--- Service-and-Factory/exfactory.js
+++ Service-and-Factory/exfactory.js
@@ -1,1 +1,19 @@
+// declared the app with no dependencies
 
+var app = angular.module('myApp', []);
+
+/* I HAVE CREATED ONE SIMPLE FACTORY "data" HERE.
+NOW, ANYTHING USER INPUTS IN THE INPUT BOX WILL BE RETRIEVED AND STORED IN THIS FACTORY.
+WE CAN USE THIS DATA IN OUR DIFFERENT CONTROLLERS, USING THIS FACTORY*/
+
+app.factory('Data', function(){
+    return { FirstName: '' };
+});
+
+app.controller('FirstCtrl', function( $scope, Data ){
+	$scope.Data = Data;
+});
+
+app.controller('SecondCtrl', function( $scope, Data ){
+	$scope.Data = Data;
+});
